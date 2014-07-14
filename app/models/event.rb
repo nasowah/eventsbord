e9a290e35df95cccb544aead40fee9f035efe719
Ernest Addae <earnestaddae@gmail.com>
2014-07-14 15:32:14 +0000

    Config with Figaro gem

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "default.gif"
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+														:default_url => "default.gif",
+														:storage => :dropbox,
+    												:dropbox_credentials => Rails.root.join("config/dropbox.yml")
 
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
