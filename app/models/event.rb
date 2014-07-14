5f7b7123c6947ca16159ed9469ca4a472fc9a38c
Ernest Addae <earnestaddae@gmail.com>
2014-07-14 15:51:58 +0000

    Complete setup for Dropbox to host images on Heroku

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,15 @@
 class Event < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+	if Rails.env.development?
+		has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+														:default_url => "default.gif"
+	else
+		has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
 														:default_url => "default.gif",
 														:storage => :dropbox,
-    												:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+    												:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    												:path => ":style/:id_:filename" 
+	end
+	
 
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
