842b56ebd97643036937615eab2de1be252fa3b5
Ernest Addae <earnestaddae@gmail.com>
2014-07-16 17:38:32 +0000

    Redsign the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def home
+  	 if params[:search]
+      @events = Event.search(params[:search]).order("created_at DESC")
+    else
+      @events = Event.all.order('created_at DESC')
+    end
   end
 
   def about
